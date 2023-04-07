7a44f30257f697ed795caea07bbe8ba9aa599051
Yoshida <61533915+naoto-ysd@users.noreply.github.com>
2023-04-07 23:59:09 +0900

    install tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,25 @@
 <template>
-  <div id="app">
-    <select v-model="selectedItem">
-      <option disabled value="">Select an item</option>
-      <option v-for="item in items" :key="item.id" :value="item">{{ item }}</option>
-    </select>
-    <button @click="addItem">Add Item</button>
-    <ul>
-      <li v-for="item in selectedItems" :key="item.id">{{ item }}</li>
-    </ul>
+  <div class="flex justify-center items-center h-screen">
+    <div class="text-center">
+      <label for="fruits">スタジオ選択</label>
+      <select id="fruits" v-model="selectedFruit"
+              class="block mx-auto rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+        <option value="">Please select</option>
+        <option v-for="(fruit, index) in fruits" :key="index" :value="fruit">{{ fruit }}</option>
+      </select>
+      <p v-if="selectedFruit">You selected {{ selectedFruit }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import { createApp, ref } from 'vue';
-
-const App = {
+export default {
   name: 'App',
-  setup() {
-    const items = ['新宿', '渋谷', '代官山', '秋葉原'];
-    const selectedItem = ref('');
-    const selectedItems = ref([]);
-
-    const addItem = () => {
-      if (selectedItem.value !== '') {
-        selectedItems.value.push(selectedItem.value);
-        selectedItem.value = '';
-      }
-    }
-
+  data() {
     return {
-      items,
-      selectedItem,
-      selectedItems,
-      addItem
-    }
+      fruits: ['Apple', 'Banana', 'Orange', 'Mango'],
+      selectedFruit: ''
+    };
   }
 };
-
-const app = createApp(App);
-app.mount('#app');
-
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false,
+  theme: {
+    extend: {
+      colors: {
+        primary: '#4F46E5',
+        secondary: '#FFC857'
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
